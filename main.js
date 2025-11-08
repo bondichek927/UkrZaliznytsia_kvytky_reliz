6dd6685703461eb124c86ca72cb967e543478210
Abembe <bondik060513@gmail.com>
2025-11-08 13:02:37 +0200

    Ер9т-732

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+// Функція для отримання значення кукі за ім'ям
+function getCookieValue(cookieName) {
+    // Розділяємо всі куки на окремі частини
+    const cookies = document.cookie.split(';');
+
+
+    // Шукаємо куки з вказаним ім'ям
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim(); // Видаляємо зайві пробіли
+
+
+        // Перевіряємо, чи починається поточне кукі з шуканого імені
+        if (cookie.startsWith(cookieName + '=')) {
+            // Якщо так, повертаємо значення кукі
+            return cookie.substring(cookieName.length + 1); // +1 для пропуску символу "="
+        }
+    }
+    // Якщо кукі з вказаним іменем не знайдено, повертаємо порожній рядок або можна повернути null
+    return '';
+}
+
 // Оголошуємо асинхронну функцію для отримання продуктів з сервера
 async function getProducts() {
     // Виконуємо запит до файлу "store_db.json" та очікуємо на відповідь
@@ -20,60 +41,100 @@ function getCardHTML(product) {
 </div>`;
 }
 
+
+
 class ShoppingCart {
-    constructor() {
-        this.items = {} // об’єкт з товарами у кошику
-        this.total = 0  // загальна вартість замовлення
-    }
+  constructor() {
+    this.items = {}; // об’єкт з товарами у кошику
+    this.total = 0; // загальна вартість замовлення
+    this.loadCartFromCookies()
+  }
 
-    addItem(item) { 
-    // Додавання товару до кошика 
-     if(this.items[item.title]){
-        this.items[item.title].quantity +=1
-     } else{
-        this.items[item.title]=item
-        this.items[item.title].quantity=1
-     }
-        
-    }  
-    	
-    saveCartToCookies() { 
-        // збереження кошика у кукі
-        let cartJSON = JSON.stringify(this.items);
-        document.cookie = `cart=${cartJSON}; max-age=${60 * 60 * 24 * 7}; path=/`;
-    }
 
-    loadCartFromCookies() { // Завантаження кошика з кукі
+  addItem(item) {
+    // Додавання товару до кошика
+    if (this.items[item.title]) {
+      this.items[item.title].quantity += 1;
+    } else {
+      this.items[item.title] = item;
+      this.items[item.title].quantity = 1;
     }
+    this.saveCartToCookies();
+    console.log(this.items);
+  }
+
+
+  saveCartToCookies() {
+    // збереження кошика у кукі
+    let cartJSON = JSON.stringify(this.items);
+    document.cookie = `cart=${cartJSON}; max-age=${60 * 60 * 24 * 7}; path=/`;
+  }
+
+
+  loadCartFromCookies() {
+    // Завантаження кошика з кукі
+    let cartCookie = getCookieValue("cart");
+    if (cartCookie && cartCookie !== "") {
+      this.items = JSON.parse(cartCookie);
+    }
+  }
 }
-let cart = new ShoppingCart() // Створення об'єкта кошика
+let cart = new ShoppingCart(); // Створення об'єкта кошика
 
-function addToCart(event){
-    // Отримуємо дані про товар з data-атрибута кнопки\
-      const productData = event.target.getAttribute('data-product') 
-      const product = JSON.parse(productData) 
-      // перетворюємо JSON у об’єкт /
-      // / Тут будемо додавати товар до кошика
-       cart.addItem(product);
+
+function addToCart(event) {
+  // Отримуємо дані про товар з data-атрибута кнопки
+  const productData = event.target.getAttribute("data-product");
+  const product = JSON.parse(productData); // перетворюємо JSON у об’єкт
+  // Тут будемо додавати товар до кошика
+  cart.addItem(product);
+  event.preventDefault();
 }
+
+
 // Викликаємо асинхронну функцію та очікуємо на отримання продуктів
 getProducts().then(function (products) {
-    let productsList = document.querySelector('.products-list')
-    if (productsList) {
-        products.forEach(function (product) {
-            // Відображаємо товари на сторінці
-            productsList.innerHTML += getCardHTML(product)
-        })
-    }
+  let productsList = document.querySelector(".products-list");
+  if (productsList) {
+    products.forEach(function (product) {
+      // Відображаємо товари на сторінці
+      productsList.innerHTML += getCardHTML(product);
+    });
+  }
 
-    
 
-    // Отримуємо всі кнопки "Купити" на сторінці
-    let buyButtons = document.querySelectorAll('.products-list .btn-primary');
-    // Навішуємо обробник подій на кожну кнопку "Купити"
-    if (buyButtons) {
-        buyButtons.forEach(function (button) {
-            button.addEventListener('click', addToCart)
-        });
-    }
-})
+  // Отримуємо всі кнопки "Купити" на сторінці
+  let buyButtons = document.querySelectorAll(".products-list .btn-primary");
+  // Навішуємо обробник подій на кожну кнопку "Купити"
+  if (buyButtons) {
+    buyButtons.forEach(function (button) {
+      button.addEventListener("click", addToCart);
+    });
+  }
+});
+
+
+
+// оформлення сторінки корзини 
+let cart_list = document.querySelector(".cart-items-list");
+function get_item(item) {
+  return `<div class = "cart-item">
+                <h4 class="cart-item-title">${item.title}</h4>
+                <div class="cart-item-quantity">Кількість: ${
+                  item.quantity
+                }</div>
+                <div class="cart-item-price" data-price="${item.price}">${
+    item.price * item.quantity
+  } грн</div>
+            </div>`;
+}
+function showCartList() {
+  //cart_list.innerHTML = "";
+  for (let key in cart.items) {
+    // проходимося по всіх ключах об'єкта cart.items
+    cart_list.innerHTML += get_item(cart.items[key]);
+  }
+  //cart_total.innerHTML = cart.calculateTotal();
+}
+
+showCartList()

5ee7ce2e7a14a870389d343e92cb1fce5c38114f
Abembe <bondik060513@gmail.com>
2025-11-15 13:30:15 +0200

    абвгд
    вівіавіі

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ async function getProducts() {
 
 function getCardHTML(product) {
     return `<div class="card" style="width: 18rem;">
-  <img src="img/${product.img}" class="card-img-top" alt="...">
+  <img src="${product.image}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${product.title}</h5>
     <p class="card-text">${product.price}</p>
